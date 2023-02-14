f9f17dd15d6fcea01535351cbd4ccb1ec2b1b174
Stakita77 <hardstylerootz@gmail.com>
2023-02-14 01:22:24 +0100

    Tarea 4

---
+++ src/main/java/cuentas/Main.java
@@ -0,0 +1,32 @@
+
+package cuentas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
+
+        operativa_cuenta(cuenta1, 2300);
+        operativa_cuenta(cuenta1, 695);
+    }
+
+    public static void operativa_cuenta(CCuenta cuenta, double cantidad) {
+        try {
+            cuenta.retirar(cantidad);
+            System.out.println("Retiro en cuenta realizado con éxito");
+        } catch (Exception e) {
+            System.out.println("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.println("Fallo al ingresar");
+        }
+    }
+}
